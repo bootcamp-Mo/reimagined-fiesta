15a6df200c6f5db8f9136c37e3aa99d13366abd4
monique <mo.sherred@gmail.com>
2023-07-15 22:47:59 -0700

    trying the front end

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,9 +7,11 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
@@ -17,29 +19,19 @@ const SavedBooks = () => {
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
+  const { loading, data: queryData } = useQuery(GET_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+
   useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (loading) {
+      return;
+    }
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
+    if (!userData && queryData) {
+      setUserData(queryData.me);
+    } else if (!loading && queryData && userData._id !== queryData.me._id) {
+      setUserData(queryData.me);
+    }
   }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -51,25 +43,27 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      const { data } = await removeBook({
+        variables: { bookId },
+        update: (cache, { data: { removeBook } }) => {
+          try {
+            const { me } = cache.readQuery({ query: GET_ME });
+            cache.writeQuery({
+              query: GET_ME,
+              data: { me: { ...me, savedBooks: removeBook.savedBooks } }
+            });
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
+      // remove the book's ID from localStorage
+      localStorage.removeItem(bookId);
     } catch (err) {
       console.error(err);
     }
   };
-
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
 
   return (
     <>
